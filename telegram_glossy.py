567fee91216d970c2078e1c959342985e2d749e4
Borisova Olga <www.doglor@gmail.com>
2022-01-14 01:31:53 +0300

    added new functions and multiusers work

--- telegram_glossy.py
+++ telegram_glossy.py
@@ -1,11 +1,7 @@
 import typing as tp
 
+from telebot import types
 import telebot
-
-
-class ClientStatus:
-    pass
-
 
 class TelegramGlossy:
     def __init__(self, token: str, dictionary):
@@ -13,7 +9,51 @@ class TelegramGlossy:
         self.history = {}
         self.dictionary = dictionary
 
-        self.clients: tp.Mapping[str, ClientStatus] = dict()
+        self.bot.set_update_listener(lambda messages: self._handle_messages(messages))
+        self.bot.register_callback_query_handler(lambda reply: self._query_handler(reply), lambda reply: True)
+
+    def _query_handler(self, reply):
+        command, word = reply.data.split(":")
+        self.bot.send_message(reply.message.chat.id, "The word was " + word + "\nAnd command was " + command)
+
+        # reply = None
+        # if command == "syn":
+        #     reply = self._get_synonyms(word)
+        # if command == not ""...
+        #
+        #
+
+    def _handle_messages(self, messages):
+        for message in messages:
+            self._handle_message(message)
+
+    def _handle_message(self, message):
+        print(message)
+
+        if message.content_type != "text":
+            self.bot.send_message(message.chat.id, "Only text content is allowed")
+
+        word = message.text
+
+        keyboard = types.InlineKeyboardMarkup(row_width=2)
+        buttons = [
+            types.InlineKeyboardButton(text='Значение', callback_data='info:' + word),
+            types.InlineKeyboardButton(text='Произношение', callback_data='pro:' + word),
+            types.InlineKeyboardButton(text="Часть речи", callback_data="part:" + word),
+            types.InlineKeyboardButton(text="Синонимы", callback_data="syn:" + word),
+            types.InlineKeyboardButton(text="Антонимы", callback_data="ant:" + word),
+            types.InlineKeyboardButton(text="Примеры", callback_data="exm:" + word)
+        ]
+
+        keyboard.add(*buttons)
+
+        self.bot.send_message(message.chat.id, text="Выбирай", reply_markup=keyboard)
+
+    def _make_keyboard(self, word: str) -> types.InlineKeyboardMarkup:
+        pass
+
+    def _get_synonyms(self, word: str) -> str:
+        pass
 
     def run(self):
         self.bot.infinity_polling()

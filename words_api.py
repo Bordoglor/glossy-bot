80aec7cd72b0231da783ea26415f1687dbe4683d
Borisova Olga <31622142+Bordoglor@users.noreply.github.com>
2022-01-18 00:40:30 +0300

    Update words_api.py

--- words_api.py
+++ words_api.py
@@ -4,8 +4,6 @@ import requests
 from urllib.parse import urljoin
 
 
-# Класс, который позволяет получить информацию о слове, запрашивая информацию у сайта
-# Также для уменьшения сетевого взаимодействия, класс имеет локальный кеш, в котором записаны уже полученные данные о словах.
 class WordsApi:
     URL = "https://wordsapiv1.p.rapidapi.com/words/"
 
@@ -87,7 +85,6 @@ class WordsApi:
         return examples
 
     def contains(self, word) -> bool:
-        # Если слова нет в ловкальной бд, то оно запрашивается с сайта. Если и там его нет, то возвращается false
         if word in self.cache:
             return True
 
@@ -108,53 +105,3 @@ class WordsApi:
         self.cache[word] = word_info["results"]
         self.pro[word] = word_info["pronunciation"]["all"]
         return True
-
-
-# class WordsApiLocal:
-#
-#     def __init__(self):
-#         self.database = {"definitions": ["def line1 ", "def line2 ", "def line3 "],
-#                          "pronunciation": "This is pronunciation of ",
-#                          "synonyms": ["syn1", "syn2", "syn3"], "antonyms": ["ant1", "ant2", "ant3"],
-#                          "partOfSpeech": ["part1", "part2", "part2", "part3"], "examples": ["ex1", "ex2", "ex3"]}
-#
-#         self.definitions = self._get_definitions(self.database)
-#         self.pronunciation = self._get_pronunciation(self.database)
-#         self.synonyms = self._get_synonyms(self.database)
-#         self.antonyms = self._get_antonyms(self.database)
-#         self.partOfSpeech = self._get_part_of_speech(self.database)
-#         self.examples = self._get_examples(self.database)
-#
-#     def _get_definitions(self, database):
-#         if "definitions" in database.keys():
-#             return database["definitions"]
-#
-#     def _get_pronunciation(self, database):
-#         if "pronunciation" in database.keys():
-#             return database["pronunciation"]
-#         else:
-#             return None
-#
-#     def _get_synonyms(self, database):
-#         if "synonyms" in database.keys():
-#             return database["synonyms"]
-#         else:
-#             return None
-#
-#     def _get_antonyms(self, database):
-#         if "antonyms" in database.keys():
-#             return database["antonyms"]
-#         else:
-#             return None
-#
-#     def _get_part_of_speech(self, database):
-#         if "partOfSpeech" in database.keys():
-#             return set(database["partOfSpeech"])
-#         else:
-#             return None
-#
-#     def _get_examples(self, database):
-#         if "examples" in database.keys():
-#             return database["examples"]
-#         else:
-#             return None
